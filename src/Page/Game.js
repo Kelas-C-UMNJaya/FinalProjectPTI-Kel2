90c6f552366a87c56a8c0e419c888dfa40839dec
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-06-01 14:29:32 +0700

    Progress bar Fixed
    Co-authored-by: Steve Christian <stevgamz@users.noreply.github.com>

--- src/Page/Game.js
+++ src/Page/Game.js
@@ -8,10 +8,16 @@ import { IoFastFoodSharp, IoGameControllerSharp, IoSchoolSharp } from 'react-ico
 function Game() {
     const { userData } = useContext(userContext);
     //USESTATE PROGRESS BAR==============//
-    const [makan, setMakan] = useState(0);
+    const [makan, setMakan] = useState(50);
     const [belajar, setBelajar] = useState(0);
-    const [main, setMain] = useState(0);
-    const [tidur, setTidur] = useState(0);
+    const [main, setMain] = useState(50);
+    const [tidur, setTidur] = useState(50);
+
+    //BUTTON BACKGROUND COLOR==============//
+    const [bgMakan, setBgMakan] = useState("#EAE0D5");
+    const [bgMain, setBgMain] = useState("#EAE0D5");
+    const [bgTidur, setBgTidur] = useState("#EAE0D5");
+    const [bgBelajar, setBgBelajar] = useState("#EAE0D5");
 
     //USESTATE BUTTON===================//
     const [button, setButton] = useState("");
@@ -20,27 +26,69 @@ function Game() {
     const [buttonMain, setButtonMain] = useState(true);
     const [buttonTidur, setButtonTidur] = useState(true);
 
+    function updateStatus(val, callback, rate, movement) {
+        if (movement === "naik" && (val + rate) <= 100) {
+            callback((prevVal) => prevVal + rate);
+            return;
+        }
+        else if (movement === "turun" && (val - rate) >= 0) {
+            callback((prevVal) => prevVal - rate);
+            return;
+        } else {
+            callback(val < 0 ? 0 : val > 100 ? 100 : val);
+            return;
+        }
+
+    }
+
     const updateStatusBar = (status) => {
         if (status === "") {
-            setMakan((prevCounter) => prevCounter - 1);
-            setMain((prevCounter) => prevCounter - 1);
-            setTidur((prevCounter) => prevCounter - 1);
+            updateStatus(makan, setMakan, 1, "turun");
+            updateStatus(main, setMain, 1, "turun");
+            updateStatus(tidur, setTidur, 0.5, "turun");
+
+            // setMakan((prevCounter) => prevCounter - 1);
+            // setMain((prevCounter) => prevCounter - 1);
+            // setTidur((prevCounter) => prevCounter - 0.5);
         }
         else if (status === "makan") {
-            setMakan((prevCounter) => prevCounter + 1);
-            setMain((prevCounter) => prevCounter - 1);
-            setTidur((prevCounter) => prevCounter - 1);
+            updateStatus(makan, setMakan, 5, "naik");
+            updateStatus(main, setMain, 1, "turun");
+            updateStatus(tidur, setTidur, 1, "turun");
+
+            // setMakan((prevCounter) => prevCounter + 5);
+            // setMain((prevCounter) => prevCounter - 1);
+            // setTidur((prevCounter) => prevCounter - 1);
+
         }
         else if (status === "main") {
-            setMakan((prevCounter) => prevCounter - 1);
-            setMain((prevCounter) => prevCounter + 1);
-            setTidur((prevCounter) => prevCounter - 1);
+            updateStatus(makan, setMakan, 1, "turun");
+            updateStatus(main, setMain, 4, "naik");
+            updateStatus(tidur, setTidur, 0.7, "turun");
+
+            // setMakan((prevCounter) => prevCounter - 1);
+            // setMain((prevCounter) => prevCounter + 4);
+            // setTidur((prevCounter) => prevCounter - 0.7);
         }
         else if (status === "belajar") {
-            setMakan((prevCounter) => prevCounter - 1);
-            setBelajar((prevCounter) => prevCounter + 1);
-            setMain((prevCounter) => prevCounter - 1);
-            setTidur((prevCounter) => prevCounter - 1);
+            updateStatus(makan, setMakan, 1, "turun");
+            updateStatus(main, setMain, 1, "turun");
+            updateStatus(tidur, setTidur, 1, "turun");
+            updateStatus(belajar, setBelajar, 2, "naik");
+
+            // setMakan((prevCounter) => prevCounter - 1);
+            // setBelajar((prevCounter) => prevCounter + 2);
+            // setMain((prevCounter) => prevCounter - 1);
+            // setTidur((prevCounter) => prevCounter - 1);
+        }
+        else if (status === "tidur") {
+            updateStatus(makan, setMakan, 1, "turun");
+            updateStatus(main, setMain, 1, "turun");
+            updateStatus(tidur, setTidur, 2, "naik");
+
+            // setMakan((prevCounter) => prevCounter - 1);
+            // setMain((prevCounter) => prevCounter - 1);
+            // setTidur((prevCounter) => prevCounter + 1);
         }
     }
 
@@ -49,10 +97,15 @@ function Game() {
         setButtonMain(true);
         setButtonTidur(true);
 
-        if (setButtonMakan) {
+        if (buttonMakan) {
             setButton("makan");
+            setBgMakan("#7678ed");
+            setBgBelajar("#EAE0D5");
+            setBgMain("#EAE0D5");
+            setBgTidur("#EAE0D5");
         } else {
             setButton("");
+            setBgMakan("#EAE0D5");
         }
         setButtonMakan(!buttonMakan);
     }
@@ -62,10 +115,15 @@ function Game() {
         setButtonMakan(true);
         setButtonTidur(true);
 
-        if (setButtonMain) {
+        if (buttonMain) {
             setButton("main");
+            setBgMain("#7678ed");
+            setBgBelajar("#EAE0D5");
+            setBgMakan("#EAE0D5");
+            setBgTidur("#EAE0D5");
         } else {
             setButton("");
+            setBgMain("#EAE0D5");
         }
         setButtonMain(!buttonMain);
     }
@@ -75,10 +133,15 @@ function Game() {
         setButtonMakan(true);
         setButtonTidur(true);
 
-        if (setButtonBelajar) {
+        if (buttonBelajar) {
             setButton("belajar");
+            setBgBelajar("#7678ed");
+            setBgMakan("#EAE0D5");
+            setBgMain("#EAE0D5");
+            setBgTidur("#EAE0D5");
         } else {
             setButton("");
+            setBgBelajar("#EAE0D5");
         }
         setButtonBelajar(!buttonBelajar);
     }
@@ -88,14 +151,18 @@ function Game() {
         setButtonMakan(true);
         setButtonBelajar(true);
 
-        if (setButtonTidur) {
+        if (buttonTidur) {
             setButton("tidur");
+            setBgTidur("#7678ed");
+            setBgMakan("#EAE0D5");
+            setBgMain("#EAE0D5");
+            setBgBelajar("#EAE0D5");
         } else {
             setButton("");
+            setBgTidur("#EAE0D5");
         }
         setButtonTidur(!buttonTidur);
     }
-
 
 
     useEffect(() => {
@@ -112,7 +179,7 @@ function Game() {
                     <Heading as='h4' py='2' size='md' >Rabu, 09:00</Heading>
                 </Flex>
             </Flex>
-            <Flex className="Body2" justifyContent='center' gap={{ base: '0', md: '50', lg: '100' }} direction={{ base: 'row', md: 'row', lg: 'row' }}>
+            <Flex className="Body2" justifyContent='center' gap={{ base: '0', md: '100', lg: '100' }} direction={{ base: 'row', md: 'row', lg: 'row' }}>
                 <Flex className="Body" justifyContent='center' align='center' gap='20px' padding={{ base: '2' }} direction='column'>
                     <Box className="UserBar" color='#EAE0D5' borderRadius='3' bg='#2f3e46' textAlign='left' direction='column' w='full' px='4' py='2' flexGrow='1'>
                         <Text fontSize='mg'>Selamat Pagi, {userData.name}</Text>
@@ -128,28 +195,28 @@ function Game() {
                 </Flex>
                 <Flex className="Body" align='center' gap='20px' padding={{ base: '2' }} direction='column'>
                     <Grid className="StatusBox" templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)', lg: 'repeat(4, 1fr)' }} justifyContent='center' align='center' borderRadius='3' bg='#2f3e46' px='4' py='2' gap='2'>
-                        <CircularProgress value={0 + belajar} color='#7678ed' thickness='16px' >
+                        <CircularProgress value={belajar} color='#7678ed' thickness='16px' >
                             <CircularProgressLabel fontSize='2xl'>
                                 <Box className="Icon" justifyContent='center' align='center' color='#EAE0D5'>
                                     <IoSchoolSharp />
                                 </Box>
                             </CircularProgressLabel>
                         </CircularProgress>
-                        <CircularProgress value={50 + main} color='#7678ed' thickness='16px' >
+                        <CircularProgress value={main} color='#7678ed' thickness='16px' >
                             <CircularProgressLabel fontSize='2xl'>
                                 <Box className="Icon" justifyContent='center' align='center' color='#EAE0D5'>
                                     <IoGameControllerSharp />
                                 </Box>
                             </CircularProgressLabel>
                         </CircularProgress>
-                        <CircularProgress value={50 + tidur} color='#7678ed' thickness='16px' >
+                        <CircularProgress value={tidur} color='#7678ed' thickness='16px' >
                             <CircularProgressLabel fontSize='2xl'>
                                 <Box className="Icon" justifyContent='center' align='center' color='#EAE0D5'>
                                     <FaBed />
                                 </Box>
                             </CircularProgressLabel>
                         </CircularProgress>
-                        <CircularProgress value={50 + makan} color='#7678ed' thickness='16px' >
+                        <CircularProgress value={makan} color='#7678ed' thickness='16px' >
                             <CircularProgressLabel fontSize='2xl'>
                                 <Box className="Icon" justifyContent='center' align='center' color='#EAE0D5'>
                                     <IoFastFoodSharp />
@@ -158,27 +225,27 @@ function Game() {
                         </CircularProgress>
                     </Grid>
                     <SimpleGrid columns={{ base: '2', md: '2' }} spacing='10px' color='#2f3e46' bg='#2f3e46' padding='3' borderRadius='3'>
-                        <Button onClick={handleClickBelajar} size='sm' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button onClick={handleClickBelajar} size='sm' bg={bgBelajar} _hover={{ bg: '#7678ed' }} variant='solid'>
                             Belajar
                         </Button>
-                        <Button onClick={handleClickMain} size='sm' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button onClick={handleClickMain} size='sm' bg={bgMain} _hover={{ bg: '#7678ed' }} variant='solid'>
                             Main
                         </Button>
-                        <Button onClick={handleClickTidur} size='sm' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button onClick={handleClickTidur} size='sm' bg={bgTidur} _hover={{ bg: '#7678ed' }} variant='solid'>
                             Tidur
                         </Button>
-                        <Button onClick={handleClickMakan} size='sm' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button onClick={handleClickMakan} size='sm' bg={bgMakan} _hover={{ bg: '#7678ed' }} variant='solid'>
                             Makan
                         </Button>
                     </SimpleGrid >
-                    <Flex flexWrap='wrap' w='180px' flexDir='row' justifyContent='center' gap='15px' color='#2f3e46' bg='#2f3e46' padding='3' borderRadius='3'>
-                        <Button size='sm' w='70px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                    <Flex flexWrap='wrap' w='175px' flexDir='row' justifyContent='center' gap='15px' color='#2f3e46' bg='#2f3e46' padding='3' borderRadius='3'>
+                        <Button size='sm' w='65px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
                             Kampus
                         </Button>
-                        <Button size='sm' w='70px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button size='sm' w='65px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
                             Hiling
                         </Button>
-                        <Button size='sm' w='70px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
+                        <Button size='sm' w='65px' bg='#EAE0D5' _hover={{ bg: '#7678ed' }} variant='solid'>
                             Cafe
                         </Button>
                     </Flex >
