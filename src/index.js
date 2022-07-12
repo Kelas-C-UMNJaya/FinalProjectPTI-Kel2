b127a2407b32369332205e11dc06ae771af033ba
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-07-13 00:28:04 +0700

    change router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import { React, useContext } from 'react';
+import { React } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import Game from './Page/Game';
 import App from './App';
 import GameOver from './Page/GameOver';
 import { UserProvider } from './UserContext';
-import { StatusProvider } from './StatusContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
     <UserProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/Game" element={<Game />} />
           <Route path="/GameOver" element={<GameOver />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </ChakraProvider>
 );
